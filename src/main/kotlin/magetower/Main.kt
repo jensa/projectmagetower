8e99a452d7715e1b7cfe73916faf8d64ea3ed2b3
Jens Arvidsson <jensarv@gmail.com>
2018-07-18 01:51:54 +0200

    remove hello world

--- src/main/kotlin/magetower/Main.kt
+++ src/main/kotlin/magetower/Main.kt
@@ -3,13 +3,6 @@ package magetower
 import kotlinx.serialization.json.JSON
 import java.io.File
 
-
-class Main(val name : String) {
-    fun greet() {
-        println("Hello, ${name}");
-    }
-}
-
 fun main(args : Array<String>) {
     val file = File("state.json")
     val stateString = if(file.exists()) file.readText() else ""
